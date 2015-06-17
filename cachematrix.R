f262e3f980179217a875accd9dd313d1fe36cae3
Vivek-B <vivekbakaraju@gmail.com>
2015-06-17 19:57:11 +0530

    The cachematrix file has been eddited and now contains the Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -20,7 +20,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-## Second Function:-
+## SECOND FUNCTION:-
 ## This function computes the inverse of the special "matrix" returned by 
 ## makeCacheMatrix above. If the inverse has already been calculated 
 ## (and the matrix has not changed), then cacheSolve should retrieve the 
